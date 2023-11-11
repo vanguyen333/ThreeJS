539739b009173b47183ff5c2a7a1e15782ff1690
vanguyen333 <vanguyen333@yahoo.com>
2023-11-11 16:18:15 -0500

    added plane

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import planeScene from "../assets/3d/plane.glb";
+import { useGLTF } from "@react-three/drei";
 
 const Plane = () => {
-  return <mesh>Plane</mesh>;
+  const { scene, animations } = useGLTF(planeScene);
+  return (
+    <mesh>
+      <primitive object={scene} />
+    </mesh>
+  );
 };
 
 export default Plane;
