181971e7d5701cb3ea28cf65c62c27191f62e886
vanguyen333 <vanguyen333@yahoo.com>
2023-11-11 15:42:14 -0500

    added asset folder, modified vite.config.js, created models with Island.jsx file, island shown on page but not positioned properly yet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+
+import Island from "../models/Island";
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
@@ -13,7 +15,14 @@ const Home = () => {
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island />
+        </Suspense>
       </Canvas>
     </section>
   );
