5ec6ebc3a71ae83fef0c14d7faf0dbc7e4d437ea
vanguyen333 <vanguyen333@yahoo.com>
2023-11-10 22:03:28 -0500

    created structure for react-router-dome in App.js

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className=" blue-gradient_text">VN</p>
+      </NavLink>
+    </header>
+  );
+};
+
+export default Navbar;
