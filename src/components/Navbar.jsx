7e381fba5ef0e57d82aa2b81b5667ad1954d7e5b
vanguyen333 <vanguyen333@yahoo.com>
2023-11-10 22:10:15 -0500

    navbar react router dom works

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,24 @@ const Navbar = () => {
       >
         <p className=" blue-gradient_text">VN</p>
       </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500 " : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500 " : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+      </nav>
     </header>
   );
 };
