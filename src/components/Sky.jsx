2331939dac6bb1401b19680d9472f84c7a1f64f6
vanguyen333 <vanguyen333@yahoo.com>
2023-11-11 21:17:28 -0500

    sky rotates

--- src/components/Sky.jsx
+++ src/components/Sky.jsx
@@ -5,6 +5,11 @@ import SkyScene from "../assets/3d/sky.glb";
 const Sky = ({ isRotating }) => {
   const sky = useGLTF(SkyScene);
   const skyRef = useRef();
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.15 * delta;
+    }
+  });
   return (
     <mesh ref={skyRef}>
       <primitive object={sky.scene} />
