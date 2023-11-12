f58d86fba28b33c0d2883f72157c04900132aed9
vanguyen333 <vanguyen333@yahoo.com>
2023-11-11 21:28:06 -0500

    bird is able to play

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,10 +1,18 @@
-import { useGLTF } from "@react-three/drei";
-import birdScene from "../assets/3d/bird.glb";
+import { useEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useAnimations, useGLTF } from "@react-three/drei";
 
+import birdScene from "../assets/3d/bird.glb";
 const Bird = () => {
-  const { scene, animation } = useGLTF(birdScene);
+  const birdRef = useRef();
+  const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, birdRef);
+
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, []);
   return (
-    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]} ref={birdRef}>
       <primitive object={scene} />
     </mesh>
   );
