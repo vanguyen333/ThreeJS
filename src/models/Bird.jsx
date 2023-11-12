6f2888c9634ef513bc88a3864b22da2c9522855b
vanguyen333 <vanguyen333@yahoo.com>
2023-11-11 21:45:35 -0500

    bird flies in and out

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -11,6 +11,25 @@ const Bird = () => {
   useEffect(() => {
     actions["Take 001"].play();
   }, []);
+
+  useFrame(({ clock, camera }) => {
+    //update the Y position simulate the flight moving in a sin wave
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+    if (birdRef.current.position.x > camera.position.x + 10) {
+      birdRef.current.rotation.y = Math.PI;
+    } else if (birdRef.current.position.x < camera.position.x - 10) {
+      birdRef.current.rotation.y = 0;
+    }
+
+    if (birdRef.current.rotation.y === 0) {
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    } else {
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
+    }
+  });
   return (
     <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]} ref={birdRef}>
       <primitive object={scene} />
