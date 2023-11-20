27dff99d2dd92ec295f3b00fa19e2290f94002c9
vanguyen333 <vanguyen333@yahoo.com>
2023-11-19 20:22:26 -0500

    added some technical icon to skills

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { skills } from "../constants";
 const About = () => {
   return (
     <section className="max-container">
@@ -17,7 +17,19 @@ const About = () => {
       </div>
       <div className="py-10 flex flex-col">
         <h3 className="subhead-text">My Skills</h3>
-        <div className="mt-16 flex flex-wrap gap-12"></div>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div>
+              <div>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
