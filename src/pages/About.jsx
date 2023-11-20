e204dddf679a160d50684773592ac37d41bc6769
vanguyen333 <vanguyen333@yahoo.com>
2023-11-19 20:27:15 -0500

    added back box shadow to skills icons

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,6 +20,7 @@ const About = () => {
         <div className="mt-16 flex flex-wrap gap-12">
           {skills.map((skill) => (
             <div className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
               <div className="btn-front rounded-xl flex justify-center items-center">
                 <img
                   src={skill.imageUrl}
