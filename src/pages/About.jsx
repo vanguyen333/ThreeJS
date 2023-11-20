5676425dfd247ebae38e3957b2ffe41d15cd7dbf
vanguyen333 <vanguyen333@yahoo.com>
2023-11-19 20:05:07 -0500

    added intro to About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Van
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Software Engineer based in Ottawa, Canada, specializing in technical
+          education through hands-on learning and building applications.
+        </p>
+      </div>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12"></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
