b7e46c7beec75625c79fd07d874c7b537ae93db3
vanguyen333 <vanguyen333@yahoo.com>
2023-12-18 16:00:11 -0500

    added description for videos in project page

--- src/components/ProjectVideos.jsx
+++ src/components/ProjectVideos.jsx
@@ -8,6 +8,18 @@ const ProjectVideos = () => {
       <h4 className="text-2xl font-poppins font-semibold">
         My First Portfolio - React JS
       </h4>
+      <p className="mt-2 text-slate-500">
+        Developed a web application that showcases my ability to learn new
+        technologies after first exposed to reactjs, vite and tailwindcss.
+        <br />
+        Source Code:
+        <a
+          href="https://github.com/vanguyen333/mysiteO"
+          className="text-blue-500"
+        >
+          Click here!
+        </a>
+      </p>
       <video
         className="py-10"
         src={FirstPortfolio}
@@ -17,6 +29,17 @@ const ProjectVideos = () => {
       <h4 className="text-2xl font-poppins font-semibold">
         Putuk Truno Camp Area - Next JS Web Aplication
       </h4>
+      <p className="mt-2 text-slate-500">
+        It's the first web application developed using NextJS that I learned
+        from a wonderful tutorial by <strong>Adrian Hajdin</strong> <br />{" "}
+        Source Code:{" "}
+        <a
+          href="https://github.com/vanguyen333/NextJS"
+          className="text-blue-500"
+        >
+          Click here!
+        </a>
+      </p>
       <video
         className="py-10"
         src={Travel}
@@ -26,6 +49,12 @@ const ProjectVideos = () => {
       <h4 className="text-2xl font-poppins font-semibold">
         Practical Driving School - React Web
       </h4>
+      <p className="mt-2 text-slate-500">
+        Live Link:{" "}
+        <a href="https://practicaldrivingschool.com/" className="text-blue-500">
+          click Here!
+        </a>
+      </p>
       <video className="py-10" src={PDS} controls="controls" autoPlay="true" />
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -27,7 +27,7 @@ const Projects = () => {
       <div className="flex flex-wrap my-20 gap-16">
         <ProjectVideos />
       </div>
-      <div className="flex flex-wrap my-20 gap-16">
+      {/* <div className="flex flex-wrap my-20 gap-16">
         {projects.map((project) => (
           <div className="lg:w-[400px] w-full key={project.name}">
             <div className="block-container w-12 h-12">
@@ -63,7 +63,7 @@ const Projects = () => {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
       <hr className="border-slate-200" />
       <CTA />
     </section>
